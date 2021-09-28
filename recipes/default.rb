9c7164046e45ba10efce78cae1392486e8649300
James Badger <james@jamesbadger.ca>
2021-09-28 17:37:53 -0600

    Install fontconfig for OpenJDK support
    This allows OpenJDK to use system fonts.
    
    An integration test to confirm that fontconfig is installed was
    added.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,32 @@
 # Recipe:: default
 #
 # Copyright:: 2021, CCADI Project Contributors, All Rights Reserved.
+
+##################
+# Preconfiguration
+##################
+
+# Install fontconfig for OpenJDK to have access to system fonts
+# See: https://blog.adoptopenjdk.net/2021/01/prerequisites-for-font-support-in-adoptopenjdk/
+yum_package %w[freetype fontconfig dejavu-sans-fonts]
+
+#################
+# Install OpenJDK
+#################
+
+
+
+# install Apache Tomcat
+
+# install Apache HTTP Server
+
+# Set up HTTPS certificates and virtual hosts for HTTP Server
+
+# Install GDAL
+
+# Install GeoServer
+
+# Install GeoServer Plugins
+
+# Auto-configure GeoServer
+
